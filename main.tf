2ec059a462dddd8e318077f2b7649a73b9c2375d
Ramkumar-21-gk <gkramkumarprajapati49@gmail.com>
2026-01-16 11:25:07 +0530

    Terraform aws iam management add password with lifecycle v:3

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.28.0"
     }
   }
@@ -22,4 +22,18 @@ output "name" {
 resource "aws_iam_user" "main" {
   for_each = toset(local.user_data[*].username)
   name     = each.value
+}
+
+resource "aws_iam_user_login_profile" "example" {
+  for_each        = aws_iam_user.main
+  user            = each.value.name
+  password_length = 12
+
+  lifecycle {
+    ignore_changes = [
+      password_length,
+      password_reset_required,
+      pgp_key,
+    ]
+  }
 }
